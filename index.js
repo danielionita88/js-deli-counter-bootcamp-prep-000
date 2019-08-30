9b0a5b138bd2a02413d857242958993951a52c4c
fortuitous-map-5512 <danielionita88@yahoo.com>
2019-08-30 16:03:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-function takeANumber(katzDeliLine,name){
+function takeANumber(katzDeliLine,name) {
   katzDeliLine.push(name);
-for (var i = 0; i< katzDeliLine.length; i++) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
-}
-
 
 function nowServing(katzDeliLine) {
-  if (katzDeliLine.length === 0) {return 'There is nobody waiting to be served!'}
-
+  if(katzDeliLine.length === 0) {
+    return `There is nobody waiting to be served!`;
+  }
+  
   return `Currently serving ${katzDeliLine.shift()}.`;
-
 }
 
-
-function currentLine(deli){
-  if(deli.length === 0) {return "The line is currently empty."}
-  
-  var line =[];
-  
-  for(var i = 0; i < deli.length; i++) {
-   line.push(`${i+1}. ${deli[i]}`)
+function currentLine(katzDeliLine){
+  if(katzDeliLine.length === 0) {
+    return 'The line is currently empty.';
   }
-  return `The line is currently: ${line.join(', ')}`
+  
+  var line=[];
+  for( var i=0; i<katzDeliLine.length; i++) {
+    line.push(`${i+1}. ${katzDeliLine[i]}`);
+  }
+  return `The line is currently: ${line.join(`, `)}`
 }
